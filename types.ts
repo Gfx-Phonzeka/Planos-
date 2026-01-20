c31a78428d5337d30f76500f6d1096e927526097
Gfx-Phonzeka <gustavofonsec@gmail.com>
2026-01-20 20:34:04 +0000

    Update types.ts

--- types.ts
+++ types.ts
@@ -5,28 +5,31 @@ export enum CameraType {
   LIGHT = 'LIGHT',
   HANDHELD = 'HANDHELD',
   SSM = 'SSM',
+  ULTRASLOW = 'ULTRASLOW', // Novo
   STEADICAM = 'STEADICAM',
   CRANE = 'CRANE',
   POLECAM = 'POLECAM',
+  PERICAM = 'PERICAM',     // Novo
   MINICAM = 'MINICAM',
   SKYCAM = 'SKYCAM',
   DRONE = 'DRONE',
   UNDERWATER = 'UNDERWATER',
   BODYCAM = 'BODYCAM',
   TRACKCAM = 'TRACKCAM',
+  PTZ = 'PTZ',             // Novo
   TEXT = 'TEXT',
   ARROW = 'ARROW',
   LINE = 'LINE'
 }
 
 export interface PlacedCamera {
-  id: string; // CORREÇÃO: ID agora é sempre string
+  id: string;
   nr?: number;
   displayId?: string | number | null;
   type: CameraType;
   x: number;
   y: number;
-  x1?: number; // Para vetores
+  x1?: number;
   y1?: number;
   x2?: number;
   y2?: number;
